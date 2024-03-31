c60e3e233131a4d5f3f370458204accff2bafb31
Panax97 <163358477+clg-admon@users.noreply.github.com>
2024-03-31 15:42:54 -0600

    5t
    5t

--- assets/vendor/php-email-form/validate.js
+++ assets/vendor/php-email-form/validate.js
@@ -55,7 +55,7 @@
 
   function displayError(thisForm, error) {
     thisForm.querySelector('.loading').classList.remove('d-block');
-    thisForm.querySelector('.error-message').innerHTML = error;
+    thisForm.querySelector('.error-message').innerHTML = 'yess';
     thisForm.querySelector('.error-message').classList.add('d-block');
   }
 
