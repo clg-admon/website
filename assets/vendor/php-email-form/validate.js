cf5d0256b87798026263836475acdb09bf986aef
Panax97 <163358477+clg-admon@users.noreply.github.com>
2024-03-31 15:41:48 -0600

    js
    js

--- assets/vendor/php-email-form/validate.js
+++ assets/vendor/php-email-form/validate.js
@@ -1,8 +1,3 @@
-/**
-* PHP Email Form Validation - v3.6
-* URL: https://bootstrapmade.com/php-email-form/
-* Author: BootstrapMade.com
-*/
 (function () {
   "use strict";
 
@@ -15,37 +10,19 @@
       let thisForm = this;
 
       let action = thisForm.getAttribute('action');
-      let recaptcha = thisForm.getAttribute('data-recaptcha-site-key');
 
       if (!action) {
         displayError(thisForm, 'The form action property is not set!');
         return;
       }
+
       thisForm.querySelector('.loading').classList.add('d-block');
       thisForm.querySelector('.error-message').classList.remove('d-block');
       thisForm.querySelector('.sent-message').classList.remove('d-block');
 
       let formData = new FormData(thisForm);
 
-      if (recaptcha) {
-        if (typeof grecaptcha !== "undefined") {
-          grecaptcha.ready(function () {
-            try {
-              grecaptcha.execute(recaptcha, { action: 'php_email_form_submit' })
-                .then(token => {
-                  formData.set('recaptcha-response', token);
-                  php_email_form_submit(thisForm, action, formData);
-                })
-            } catch (error) {
-              displayError(thisForm, error);
-            }
-          });
-        } else {
-          displayError(thisForm, 'The reCaptcha javascript API url is not loaded!')
-        }
-      } else {
-        php_email_form_submit(thisForm, action, formData);
-      }
+      php_email_form_submit(thisForm, action, formData);
     });
   });
 
@@ -83,3 +60,4 @@
   }
 
 })();
+
