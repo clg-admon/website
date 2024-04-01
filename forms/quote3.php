47f71228bfa8e68dc31f33956267fe6b7f93378c
Panax97 <163358477+clg-admon@users.noreply.github.com>
2024-03-31 18:25:26 -0600

    deded
    deeded

--- forms/quote3.php
+++ forms/quote3.php
@@ -74,18 +74,11 @@ try {
     $mail->Body = $message;
 
     $result = $mail->send();
-    
-    // Muestra un mensaje emergente si el correo se envió correctamente
-    if ($result) {
-        echo '<script>alert("Correo enviado correctamente.");</script>';
-    } else {
-        echo '<script>alert("Error al enviar el correo. Por favor, inténtalo de nuevo.");</script>';
-    }
 
     // Redirige al usuario al home page
-    echo '<script>window.location.href = "../index.html";</script>';
+    echo '<script>window.location.href = "../confirmacion.html";</script>';
 
-    
+
 } catch (Exception $e) {
     echo 'Error sending the email: ', $mail->ErrorInfo;
 }
